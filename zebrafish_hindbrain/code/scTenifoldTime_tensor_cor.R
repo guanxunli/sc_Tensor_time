d8cbc16b78b368bb044240d2c9450dd75d9aa017
Guanxun Li <victorguanxun@gmail.com>
2021-03-11 16:38:04 -0600

    Add zebrafish hindbrain data set.

---
+++ zebrafish_hindbrain/code/scTenifoldTime_tensor_cor.R
@@ -0,0 +1,66 @@
+library(Matrix)
+library(RSpectra)
+
+#### load data and packages
+setwd("/data/victorxun/scTenifoldtime/zebrafish_hindbrain")
+source("new_Normalization.R")
+source("pcNet.R")
+source("regression_fun.R")
+source("tensorDecomposition.R")
+source("UMAP_order.R")
+source("scTenifoldTime.R")
+
+######## For group 2
+dta <- readRDS("data/dta_small.rds")
+sudotime <- readRDS("data/sudotime_small.rds")
+dta <- scTenifoldNet::scQC(as.matrix(dta)) 
+dta <- new_Normalization(dta)
+n_cell <- ncol(dta)
+n_gene <- nrow(dta)
+res <- list()
+
+#### Now consider scTenifoldTime
+dta_list <- list()
+time_vec <- rep(NA, 5)
+for (i in 1:4) {
+  dta_list[[i]] <- as.matrix(dta[, (250 * (i - 1) + 1):(250 * (i + 1))])
+  time_vec[i] <- mean(sudotime[colnames(dta_list[[i]]), 1])
+}
+dta_list[[5]] <- as.matrix(dta[, 1000:n_cell])
+time_vec[5] <- mean(sudotime[colnames(dta_list[[5]]), 1])
+time_vec <- time_vec / max(time_vec)
+
+#### correlation method
+res_small <- scTenifoldTime_tensor(dta_list = dta_list, time_vec = time_vec, method = "cor", nComp = 5, q = 0,
+                                   K = 5, maxIter = 10000, maxError = 1e-5, thres = 0.05, nDecimal = 2,
+                                   ma_nDim = 3)
+res$res_small <- res_small
+rm(res_small)
+
+######### For all cells
+dta <- readRDS("data/dta_large.rds")
+sudotime <- readRDS("data/sudotime_large.rds")
+dta <- scTenifoldNet::scQC(as.matrix(dta)) 
+dta <- new_Normalization(dta)
+n_cell <- ncol(dta)
+n_gene <- nrow(dta)
+
+#### Now consider scTenifoldTime
+dta_list <- list()
+time_vec <- rep(NA, 10)
+for (i in 1:9) {
+  dta_list[[i]] <- as.matrix(dta[, (500 * (i - 1) + 1):(500 * (i + 1))])
+  time_vec[i] <- mean(sudotime[colnames(dta_list[[i]]), 1])
+}
+dta_list[[10]] <- as.matrix(dta[, 4500:n_cell])
+time_vec[10] <- mean(sudotime[colnames(dta_list[[10]]), 1])
+time_vec <- time_vec / max(time_vec)
+
+#### correlation method
+res_large<- scTenifoldTime_tensor(dta_list = dta_list, time_vec = time_vec, method = "cor", nComp = 5, q = 0,
+                                  K = 5, maxIter = 10000, maxError = 1e-5, thres = 0.05, nDecimal = 2,
+                                  ma_nDim = 3)
+res$res_large <- res_large
+rm(res_large)
+
+saveRDS(res, "results/beta_pcnet.rds")
---
+++ zebrafish_hindbrain/scTenifoldTime.R
@@ -0,0 +1,125 @@
+source("pcNet.R")
+source("tensor_utility.R")
+source("tensorDecomposition.R")
+source("regression_fun.R")
+
+# dta_list is the list for dta
+# time_vec is the time vector for data list
+# nComp is # of pc for PC regression
+# q is the quntitle for pcNet
+# K is # of PC in tensor decomposition
+scTenifoldTime_beta <- function(dta_list, method = "pcnet", time_vec, nComp = 5, q = 0,
+                           K = 10, maxIter = 10000, maxError = 1e-5){
+  res <- list()
+  n_net <- length(dta_list)
+  nGenes <- nrow(dta_list[[1]])
+  gene_name <- rownames(dta_list[[1]])
+  ## generate network
+  network_list <- list()
+  for (i in seq_len(n_net)) {
+    if (method == "pcnet"){
+      network_list[[i]] <- pcNet(dta_list[[i]], nComp = nComp, scaleScores = TRUE, symmetric = FALSE, q = q, verbose = TRUE)
+      network_list[[i]] <- round(network_list[[i]], 2)
+    } else{
+      network_list[[i]] <- cor(t(dta_list[[i]]), method = "spearman")
+      network_list[[i]] <- round(network_list[[i]], 2)
+    }
+    rownames(network_list[[i]]) <- gene_name
+    print(paste0("Finish network ", i))
+  }
+  res$network_list <- network_list
+  ## tensor decomposition
+  network_tensor <- tensorDecomposition(network_list, K = K, maxIter = maxIter, maxError = maxError)
+  for (i in seq_len(length(network_tensor))) {
+    diag(network_tensor[[i]]) <- 1
+  }
+  res$network_tensor <- network_tensor
+  print("Finish tensor decomposition part.")
+
+  ## Do regression
+  res_regression <- my_regression(network_list = network_list, time_vec = time_vec)
+  beta_mat <- res_regression$beta_mat
+  t_mat <- res_regression$t_mat
+  rm(res_regression)
+  res$beta_mat <- beta_mat
+  res$t_mat <- t_mat
+  
+  ## Do regression
+  res_regression <- my_regression(network_list = network_tensor, time_vec = time_vec)
+  beta_mat <- res_regression$beta_mat
+  t_mat <- res_regression$t_mat
+  rm(res_regression)
+  res$beta_mat_tensor <- beta_mat
+  res$t_mat_tensor <- t_mat
+
+  ## return results
+  return(res)
+}
+
+scTenifoldTime_tensor <- function(dta_list, time_vec, method = "pcnet", nComp = 5, q = 0,
+                           K = 10, maxIter = 10000, maxError = 1e-5, thres = 0.05, nDecimal = 2,
+                           ma_nDim = 3, scoreType = "pos", eps = 0){
+  res <- list()
+  n_net <- length(dta_list)
+  nGenes <- nrow(dta_list[[1]])
+  gene_name <- rownames(dta_list[[1]])
+  ## generate network
+  network_list <- list()
+  for (i in seq_len(n_net)) {
+    if (method == "pcnet"){
+      network_list[[i]] <- pcNet(dta_list[[i]], nComp = nComp, scaleScores = TRUE, symmetric = FALSE, q = q, verbose = TRUE)
+      network_list[[i]] <- round(network_list[[i]], 2)
+    } else{
+      network_list[[i]] <- cor(t(dta_list[[i]]), method = "spearman")
+      network_list[[i]] <- round(network_list[[i]], 2)
+    }
+    rownames(network_list[[i]]) <- gene_name
+    print(paste0("Finish network ", i))
+  }
+  res$network_list <- network_list
+  ## tensor decomposition
+  set.seed(1)
+  tensor_output <- tensorDecomposition_time(network_list, K = K, maxError = maxError, maxIter = maxIter, 
+                                            time_vec = time_vec, thres = thres, nDecimal = nDecimal)
+  res$tensor_output <- tensor_output
+  print("Finish tensor decomposition part.")
+  
+  if (is.null(tensor_output$network0) == TRUE){
+    return(res)
+  } else{
+    ## Split of tensor output
+    tX <- as.matrix(tensor_output$network0)
+    tY <- as.matrix(tensor_output$network1)
+    rownames(tX) <- colnames(tX) <- gene_name
+    rownames(tY) <- colnames(tY) <- gene_name
+    
+    ## Making it symmetric to fulfill the requirements of the MA
+    tX <- (tX + t(tX))/2
+    tY <- (tY + t(tY))/2
+    
+    ## Non-linear manifold alignment
+    set.seed(1)
+    mA <- scTenifoldNet::manifoldAlignment(tX , tY, d = ma_nDim)
+    res$mA <- mA
+    mA_X <- mA[1:nGenes, ]
+    mA_Y <- mA[-(1:nGenes), ]
+    print("Finish manifold alignment part.")
+    
+    ## return order of gene
+    gene_diff <- rowSums((mA_X - mA_Y)^2)
+    names(gene_diff) <- gene_name
+    gene_diff <- sort(gene_diff, decreasing = TRUE)
+    res$gene_diff <- gene_diff
+    
+    # library(fgsea)
+    # BIOP <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=BioPlanet_2019')
+    # set.seed(1)
+    # E <- fgseaMultilevel(BIOP, gene_diff[!grepl('^RPL|^RPS|^RP[[:digit:]]|^MT-',names(gene_diff))],
+    #                      scoreType = scoreType, eps = eps)
+    # E <- E[order(E$pval, decreasing = FALSE),]
+    # E$leadingEdge <- unlist(lapply(E$leadingEdge, function(X){paste0(X, collapse = ';')}))
+    # res$E <- E
+    
+    return(res)
+  }
+}
